8f05853a461168f3de0fbfdd89be4efe31000fe9
andre9277 <110822583+andre9277@users.noreply.github.com>
2022-08-18 22:40:48 +0100

    Projeto finalizado
    Foi implementada toda a lógica, número de tentativas defenido para no máximo 3. O jogador também terá dicas caso não acerte no número na primeira tentativa (palpite alto ou baixo). Caso o jogador não encontre o número no final das tentativas a página dá refresh e o jogador pode voltar a jogar.

---
+++ app.js
@@ -0,0 +1,49 @@
+//numero de tentativas máximas
+let tentaMax = 3;
+
+let tentRestSpan = document.getElementById('user-trys');
+let resultP = document.getElementById('output-text');
+let palpP = document.getElementById('palpite-text');
+
+let numInput = document.getElementById('value-sub');//retorna o que o user escrever
+let btnInput = document.getElementById('btn-sub');
+
+//variável que terá um número
+let number = [Math.floor(Math.random()*100)];
+
+//Vai fazendo refresh as tentativas que o jogador tem!!
+function tentativas(numtent,number){
+    if( numtent > 0)
+        tentRestSpan.innerHTML = numtent;
+    else 
+        location.reload(); //se o numero de tentativas exceder a página dá refresh
+}
+
+
+function verificaNum() {
+    let input = document.getElementById('value-sub').value;
+
+    if(input == number){
+        palpP.innerHTML = `Acertaste, o número era o ${number}`;
+    }
+    else if (input < number){
+        palpP.innerHTML = "Palpite baixo!";
+        tentaMax--;
+        tentativas(tentaMax,number);
+    }
+
+    else if (input > number){
+        palpP.innerHTML = "Palpite alto!";
+        tentaMax--;
+        tentativas(tentaMax,number);
+    }
+    console.log(number);
+}
+
+function main(){
+    btnInput.addEventListener('click', function(){
+        verificaNum();
+    });
+}
+
+main();
